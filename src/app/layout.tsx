08401e6685d5c33d8df43de4ae11bfc8aba4d78d
M7ilan <distny2004@gmail.com>
2023-11-27 10:51:21 +0300

    small layout change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
 				<Toaster position="top-center" reverseOrder={false} />
 				<MainProvider>
 					<Navbar />
-					<main className="mt-8 px-16 m-auto pb-[50px]">{children}</main>
+					<main className="mt-8 px-16 m-auto pb-16">{children}</main>
 					{/* <Footer /> */}
 				</MainProvider>
 			</body>
