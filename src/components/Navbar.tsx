a1007e7f2c3b912cd8808cada26c52dff9c3c016
M7ilan <distny2004@gmail.com>
2023-12-01 12:15:35 +0300

    Add "Try Beta" Button

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,10 +5,14 @@ import Logo from "./Logo";
 import clsx from "clsx";
 import SwitchThemeButton from "./SwitchThemeButton";
 import Socials from "./Socials";
+import Link from "next/link";
 
 function NavbarElements() {
 	return (
 		<>
+			<Link href={"https://beta.d2lore.com"} className="btn-0">
+				TRY BETA
+			</Link>
 			<Socials />
 			<SwitchThemeButton />
 		</>
