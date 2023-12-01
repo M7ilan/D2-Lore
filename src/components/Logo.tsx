c3a72287272a0c715fb47810923f0cfe94789340
M7ilan <distny2004@gmail.com>
2023-12-01 12:24:10 +0300

    Balancing Themes

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Logo({ classname }: { classname?: string }) {
 	return (
 		<Link href="/" className={`flex items-center gap-4 text-4xl font-bold cursor-pointer ${classname}`}>
-			<Image quality={100} priority src="/Lore Logo.png" className="w-12 h-12 invert opacity-80 dark:invert-0" width={800} height={800} alt="Logo" />
+			<Image quality={100} priority src="/Lore Logo.png" className="w-12 h-12 invert opacity-80 dark:opacity-100 dark:invert-0" width={800} height={800} alt="Logo" />
 			<div>D2 Lore</div>
 		</Link>
 	);
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -14,10 +14,10 @@ export default function Sidebar() {
 		<div className="relative md:border-b border-default">
 			<div className="grid max-md:grid-cols-3 w-full gap-4 md:w-16 md:sticky md:top-10">
 				<Link href={"/books"} className={clsx("node h-16", { "active-node": isActive("/books") })}>
-					<Image className="max-w-12 max-h-12 object-contain invert dark:invert-0 !opacity-75" alt="Lore Books" src="/Lore Logo.png" width={80} height={80} />
+					<Image className="max-w-12 max-h-12 object-contain invert dark:invert-0" alt="Lore Books" src="/Lore Logo.png" width={80} height={80} />
 				</Link>
 				<Link href={"/armor"} className={clsx("node h-16", { "active-node": isActive("/armor") })}>
-					<Image className="max-w-14 max-h-14 object-contain invert dark:invert-0 !opacity-75" alt="Armors" src="/Armors.png" width={80} height={80} />
+					<Image className="max-w-14 max-h-14 object-contain invert dark:invert-0" alt="Armors" src="/Armors.png" width={80} height={80} />
 				</Link>
 				<Link href={"/bookmark"} className={clsx("node h-16", { "active-node": isActive("/bookmark") })}>
 					<HiOutlineBookmark className="max-w-12 max-h-12 w-full h-full object-contain" />
